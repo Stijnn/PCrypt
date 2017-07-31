80296f33090a8b580d2f0acd9c04d4c34688bfd1
Stijn <stijn.verhelpen@live.nl>
2017-07-31 14:03:27 +0200

    Start of implementation of networking

---
+++ PCrypt/PCrypt/Source/Handlers/UserHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace PCrypt.Source.Handlers
+{
+
+
+    class UserHandler
+    {
+        private static PCrypt.Source.Structs.PCryptUser uuid;
+        private static UserHandler instance;
+
+        private UserHandler()
+        {
+
+        }
+
+        public static AUTH_RESULT Authenticate(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(uuid))
+            {
+
+            }
+            else
+                return AUTH_RESULT.Failed;
+        }
+
+        public static void Logout()
+        {
+            uuid = null;
+        }
+    }
+
+    enum AUTH_RESULT { Succesfull, Failed }
+}
---
+++ PCrypt/PCrypt/Source/Structs/PCryptUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCrypt.Source.Structs
+{
+    class PCryptUser
+    {
+        private string uuid;
+        private string Username;
+
+        public PCryptUser(string uniqueidentifier)
+        {
+            this.uuid = uniqueidentifier;
+
+            SetupUser();
+        }
+
+        private void SetupUser()
+        {
+            
+        }
+    }
+}
